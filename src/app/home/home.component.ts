e5a5313b4eebf06471c8cff6e154df54d75a02a9
Jaynil1611 <g.jaynil2401@gmail.com>
2019-04-15 17:34:09 +0530

    MONDAYFINAL

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,9 +15,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(){
     this.isLogged=JSON.parse(localStorage.currentUser);
-  constructor(private auctionService:AuctionService) { }
-
-  ngOnInit() {
+    console.log(this.isLogged);
     this.auctionService.messages.subscribe(
       msg=>{
         switch (msg["type"]) {
